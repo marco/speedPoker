08134693cdf9302c3d1f9e5fe53d408ace41207c
Skunk <skunkmbtech@gmail.com>
2016-11-30 08:08:34 -0800

    Create the UI for the opening screen
    Create the user interface for the `MessagesViewController`, which is the
    first view controller shown when the user opens the app. Inside of it,
    add a woodgrain background, two `UIButton`s that say "Play" and "Learn",
    and a `UILabel` that says "speedPoker". Although no items should be
    interactive, they will be in the future.

--- speedPoker/MessagesExtension/MessagesViewController.swift
+++ speedPoker/MessagesExtension/MessagesViewController.swift
@@ -11,15 +11,95 @@
 import UIKit
 import Messages
 
-/// The root view controller that the Messages app uses
+/// The root view controller that the Messages app uses.
 class MessagesViewController: MSMessagesAppViewController {
     // MARK: - Properties
+    /**
+        A `UIButton` that says "Play". When touched up inside, the
+                `playButtonTouchUpInside(sender:)` method is called.
+    */
+    @IBOutlet var playButton: UIButton!
+
+    /**
+        A `UIButton` that says "Learn". When touched up inside, the
+                `learnButtonTouchUpInside(sender:)` method is called.
+    */
+    @IBOutlet var learnButton: UIButton!
+
+    /// Errors caused when attempting to autoshrink `UILabel`s.
+    enum AutoshrinkError: Error {
+        /**
+            Indicates that a `UIButton` did not have an available `titleLabel`
+                    to shrink.
+
+            - Parameters:
+                - button: The `UIButton` that had a `titleLabel` that could not
+                        be autoshrunk.
+        */
+        case noTitleLabel(button: UIButton)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        try! autoshrinkTitleLabels(of: [playButton, learnButton],
+                withMinumumScale: 0.01)
+
+        roundCorners(of: [playButton, learnButton], withAmountRounded: 0.25)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+
+    // MARK: - UI Setup
+    /**
+        Autoshrinks the `titleLabel`s of an `Array` of `UIButton`s, with a
+                selected `minimumScaleFactor`.
+
+        - Parameters:
+            - buttons: The `Array` of `UIButton`s to have their `titleLabel`s
+                    autoshrank.
+            - minimumScale: The `minimumScaleFactor` to be used when shrinking
+                    the `titleLabel`s. In other words, the smallest font
+                    size that the `titleLabel` can have is equal to
+                    (`minimumScale`×`titleLabel`).
+    */
+    func autoshrinkTitleLabels(of buttons: [UIButton],
+            withMinumumScale minimumScale: CGFloat) throws {
+        view.layoutIfNeeded()
+        for button in buttons {
+            guard button.titleLabel != nil else {
+                throw AutoshrinkError.noTitleLabel(button: button)
+            }
+
+            button.titleLabel!.adjustsFontSizeToFitWidth = true
+            button.titleLabel!.numberOfLines = 0
+            button.titleLabel!.lineBreakMode = NSLineBreakMode.byTruncatingTail
+
+            button.titleLabel!.baselineAdjustment
+                    = UIBaselineAdjustment.alignCenters
+        }
+    }
+
+    /**
+        Rounds the corners of an `Array` of `UIButton`s with a selected radius.
+
+        - Parameters:
+            - buttons: The `Array` of `UIButton`s to be rounded.
+            - amountRounded: The fraction of half of the width of the
+                    `UIButton` used as the radius of the corners. In other
+                    words, the radius of the circles used to round the
+                    `UIButton` `foo`'s corners is equal to
+                    (((`foo.width`)÷2)×`amountRounded`).
+    */
+    func roundCorners(of buttons: [UIButton],
+            withAmountRounded amountRounded: CGFloat) {
+        for button in buttons {
+            let buttonWidthHalved = button.frame.size.width / 2
+            button.layer.cornerRadius = buttonWidthHalved / 2 * amountRounded
+            button.clipsToBounds = true
+        }
     }
 
     // MARK: - Conversation Handling
